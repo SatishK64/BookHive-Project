32b3354fe3e3cc76325a3ac869051da94c1808ec
SatishK64 <skarra4@gitam.in>
2024-10-15 20:40:47 +0530

    add search

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
-import SplHeader from './Components/SplHeader';
+import SplHeader from './Components/SplHeader.jsx';
+import Footer from './Components/Footer.jsx';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './routes/Home.jsx';
+import Find from './routes/Find.jsx';
+import Lend from "./routes/Adder.jsx";
 
 function App() {
   return (
     <div className="App">
-      <SplHeader />
+      <Router>
+        <SplHeader />
+        <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/Rent" element={<Find />} />
+        <Route path="/Lend" element={<Lend />} />
+        <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
--- src/Components/SplHeader.jsx
+++ src/Components/SplHeader.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import './SplHeader.css';
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 
 function SplHeader(){
-
     return(
         
             <header>
                 <div className="Header_bar">
             
-                <div className="Header-name">BookHive</div>
-                <div className="Header-lend">Lend Books</div>
-                <div className="Header-rent">Rent Books</div>
-                    
+                <Link to= '/home' className="hyper"><div className="Header-name">BookHive</div></Link>
+                <Link to= '/Lend' className="hyper"> <button type="button" className="Header-lend">Lend Books</button></Link>
+                <Link to= '/Rent' className="hyper"><button type="button" className="Header-rent">Rent Book</button></Link>
                 </div>
             </header>
         
@@ -20,3 +20,7 @@ function SplHeader(){
 }
 
 export default SplHeader;
+export const useSearch = () => {
+    const [search, setSearch] = useState("");
+    return { search, setSearch };
+};
